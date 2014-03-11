086cd81c4870a4ddbf07178534a9901353b1e8d6
Tom Christie <tom@tomchristie.com>
2014-03-11 23:54:54 +0000

    Use coreapi's parsers

--- flask_api/parsers.py
+++ flask_api/parsers.py
@@ -1,33 +1,13 @@
 # coding: utf8
 from __future__ import unicode_literals
-from coreapi import exceptions
-from flask._compat import text_type
+from coreapi import exceptions, parsers
 from werkzeug.formparser import MultiPartParser as WerkzeugMultiPartParser
 from werkzeug.formparser import default_stream_factory
 from werkzeug.urls import url_decode_stream
-import json
 
 
-class BaseParser(object):
-    media_type = None
-    handles_file_uploads = False  # If set then 'request.files' will be populated.
-    handles_form_data = False     # If set then 'request.form' will be populated.
-
-    def parse(self, stream, media_type, **options):
-        msg = '`parse()` method must be implemented for class "%s"'
-        raise NotImplementedError(msg % self.__class__.__name__)
-
-
-class JSONParser(BaseParser):
-    media_type = 'application/json'
-
-    def parse(self, stream, media_type, **options):
-        data = stream.read().decode('utf-8')
-        try:
-            return json.loads(data)
-        except ValueError as exc:
-            msg = 'JSON parse error - %s' % text_type(exc)
-            raise exceptions.ParseError(msg)
+BaseParser = parsers.BaseParser
+JSONParser = parsers.JSONParser
 
 
 class MultiPartParser(BaseParser):
@@ -44,13 +24,12 @@ class MultiPartParser(BaseParser):
             raise exceptions.ParseError(msg)
         boundary = boundary.encode('ascii')
 
-        content_length = options.get('content_length')
-        assert content_length is not None, 'MultiPartParser.parse() requires `content_length` argument'
+        content_length = options['content_length']
 
         try:
             return multipart_parser.parse(stream, boundary, content_length)
         except ValueError as exc:
-            msg = 'Multipart parse error - %s' % text_type(exc)
+            msg = 'Multipart parse error - %s' % exc
             raise exceptions.ParseError(msg)
 
 
